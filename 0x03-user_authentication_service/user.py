3f1e0a3d184ae37a569731f868ad7dd67cfe8138
girmesh03 <girmachewzewdie03@gmail.com>
2023-09-13 23:47:00 +0300

    task 0 is added #1

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    User model for the 'users' table.
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+if __name__ == "__main__":
+    print(User.__tablename__)
+
+    for column in User.__table__.columns:
+        print("{}: {}".format(column, column.type))
